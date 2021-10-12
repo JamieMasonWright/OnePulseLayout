4d573aeb90a4cc28873fe7f535779a7e0652a399
javajj <bluegigsolutions@gmail.com>
2021-10-12 16:31:08 +0100

    Initial commit

---
+++ app/src/main/java/com/jj/onepulselayout/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.jj.onepulselayout
+
+import android.os.Bundle
+import android.view.Menu
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
